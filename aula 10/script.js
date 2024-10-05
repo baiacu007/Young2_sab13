90d9fc0f74d8d11d25c84a0ee0c1e31649fdf86a
baia <gahgamer2410@gmail.com>
2024-10-05 15:32:31 -0300

    aula 10

---
+++ aula 10/script.js
@@ -0,0 +1,43 @@
+const  CUESTONNIES_QUIZ = [
+    {
+        cuestaon:'uga ugauga ugabo',
+        a:'ugauga',
+        b: 'ga bo',
+        c: 'u',
+        d: 'uga',
+        correta:'c',
+    },
+    {
+        cuestaon:'pó pô pó',
+        a:'pó pô',
+        b:'pó',
+        c:'pòpô',
+        d:'pô',
+        correta:'c',
+    },
+    {
+        cuestaon:'?',
+        a:'!',
+        b:'&',
+        c:'$',
+        d:'@',
+        correta:'d',
+    },
+    {
+        cuestaon:'Melhor zelda é',
+        a:'c',
+        b:'ocarina of time',
+        c:'d',
+        d:'b',
+        correta:'b',
+    }
+]
+
+const TITULO =document.querySelector("h1")
+const ALT_A =document.querySelector("#texto_a")
+const ALT_B =document.querySelector("#texto_b")
+const ALT_C =document.querySelector("#texto_c")
+const ALT_D =document.querySelector("#texto_d")
+const BOTAO =document.querySelector("button")
+const CARUTAO =document.querySelector(".container")
+const ALTERNA = document.querySelector(".resposta")
